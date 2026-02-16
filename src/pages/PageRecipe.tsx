90e31113e1202dd5b8ad8312120fe0da90c7ab36
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-02-16 23:53:46 +0100

    fetch single meal

--- src/pages/PageRecipe.tsx
+++ src/pages/PageRecipe.tsx
@@ -1,6 +1,27 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import type { MealApiResponse } from "../models/interfaces";
+
 const PageRecipe = () => {
-  return (
-    <div>PageRecipe</div>
-  )
-}
-export default PageRecipe
+    const { id } = useParams();
+    const { data, error, loading } = useFetch<MealApiResponse>(
+        `${import.meta.env.VITE_BASE_API}/lookup.php?i=${id}`,
+    );
+
+    const meals = data?.meals ?? [];
+    
+    if (loading)
+        return (
+          <p>Loading...</p>
+        );
+    if (error) return <p className="text-red-500">{error}</p>;
+    if (meals.length === 0) return <p>No single meal found.</p>;
+
+    return (
+        <section>
+            <h1 className="text-3xl font-bold mb-6 text-center">{meals[0].strMeal}</h1>
+            <p>{meals[0].strInstructions}</p>
+        </section>
+    );
+};
+export default PageRecipe;
