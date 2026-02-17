b77737dc96b4a7459efbcf1dfaf31795ff70d123
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-02-17 16:35:30 +0100

    replace links in Header wiith React's client-side Link component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
   return (
     <header className="sticky top-0 bg-white z-10">
         <div className="container mx-auto px-5 py-3 flex justify-between items-center">
-            <span>COOKBIRD</span>
+            <Link to="/" className="text-2xl">
+              <span>COOKBIRD</span>
+            </Link>
             <nav>
                 <ul className="flex max-md:flex-col gap-5">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/favorites">Favorites</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/favorites">Favorites</Link></li>
                 </ul>
             </nav>
         </div>
