c2e0d4b94287803ffd5acb088ae88fa0f41f533c
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-02-25 13:44:50 +0100

    fetch all saved meals in 'Favourites' page

--- src/pages/PageSavedMeals.tsx
+++ src/pages/PageSavedMeals.tsx
@@ -1,8 +1,44 @@
+import MealCard from "@/components/meals/MealCard";
+import { useFavourites } from "@/hooks/useFavourites";
+import type { Meal } from "@/models/interfaces";
+import { useEffect, useState } from "react";
+
 const PageSavedMeals = () => {
+  const { favourites } = useFavourites();
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  // Fetch all saved meals simultaneously
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const results = await Promise.all(
+        favourites.map((id) =>
+          fetch(`${import.meta.env.VITE_BASE_API}/lookup.php?i=${id}`)
+            .then((res) => res.json())
+            .then((data) => data.meals[0])
+        )
+      );
+  
+      setMeals(results);
+    };
+
+    fetchMeals();
+  }, [favourites]);
+
   return (
     <section className="flex flex-col gap-8 max-w-5xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6 text-center">Saved Meals</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">
+        Saved Meals
+      </h1>
+
+      {meals.length === 0 && <p>No saved meals yet.</p>}
+
+      <div className="grid grid-cols-2 gap-4">
+        {meals.map((meal) => (
+          <MealCard key={meal.idMeal} meal={meal} saved={true} handleClick={() => {}} />
+        ))}
+      </div>
     </section>
-  )
-}
-export default PageSavedMeals
+  );
+};
+
+export default PageSavedMeals;
