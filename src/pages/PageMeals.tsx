56626305d18794fe87c3f84a167415270eb92fd7
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-02-26 14:02:07 +0100

    add search by name function

--- src/pages/PageMeals.tsx
+++ src/pages/PageMeals.tsx
@@ -2,33 +2,35 @@ import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import type { MealApiResponse } from "../models/interfaces";
 
-import CategoryList from "../components/categories/CategoryList";
 import MealsList from "../components/meals/MealsList";
+import Filters from "@/components/filter/Filters";
 
 const PageMeals = () => {
-const [selectedCategory, setSelectedCategory] = useState<string>("Beef");
+  const [selectedCategory, setSelectedCategory] = useState<string>("Beef");
+  const [searchName, setSearchName] = useState<string>("");
 
-  const { data, error, loading } = useFetch<MealApiResponse>(
-    `${import.meta.env.VITE_BASE_API}/filter.php?c=${selectedCategory}`
-  );
+  const endpoint = searchName.trim()
+    ? `${import.meta.env.VITE_BASE_API}/search.php?s=${searchName}`
+    : `${import.meta.env.VITE_BASE_API}/filter.php?c=${selectedCategory}`;
+
+  const { data, error, loading } = useFetch<MealApiResponse>(endpoint);
 
   const meals = data?.meals ?? [];
 
   return (
     <>
-        <h1 className="text-3xl font-bold mb-6 text-center">{`${selectedCategory} Meals`}</h1>
+      <h1 className="text-3xl font-bold mb-6 text-center">Meals</h1>
 
-        <div className="grid sm:grid-cols-[minmax(200px,1fr)_3fr] gap-8">
-          <div>
-            <h3 className="text-lg font-bold">Categories</h3>
-            <CategoryList
-              selectedCategory={selectedCategory}
-              setSelectedCategory={setSelectedCategory}
-            />
-          </div>
+      <div className="grid sm:grid-cols-[minmax(200px,1fr)_3fr] gap-8 items-start">
+        <Filters
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          searchName={searchName}
+          setSearchName={setSearchName}
+        />
 
-          <MealsList meals={meals} loading={loading} error={error} />
-        </div>
+        <MealsList meals={meals} loading={loading} error={error} />
+      </div>
     </>
   )
 }
