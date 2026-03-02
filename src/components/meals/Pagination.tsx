a29fbad27cafa0824c1456e7b8131a5cc2054dd1
nemesnyik.zsolt <zsolt.nemesnyik@webery.com>
2026-03-02 19:23:51 +0100

    change pagination colors

--- src/components/meals/Pagination.tsx
+++ src/components/meals/Pagination.tsx
@@ -1,3 +1,5 @@
+const commonClasses = "cursor-pointertransition-all disabled:opacity-10 disabled:cursor-not-allowed px-3 py-2 text-black";
+
 const Pagination = ({
     currentPage,
     totalPages,
@@ -12,9 +14,9 @@ const Pagination = ({
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
 
     return (
-        <div className="pagination-container flex items-center bg-black/15 backdrop-blur-md rounded-full overflow-hidden">
+        <div className="pagination-container flex items-center rounded-full overflow-hidden">
             <button
-                className="px-3 py-2 bg-black text-white disabled:bg-transparent disabled:text-black/10 disabled:cursor-not-allowed"
+                className={`${commonClasses}`}
                 disabled={currentPage === 1}
                 onClick={() => onPageChange(currentPage - 1)}
             >
@@ -25,8 +27,8 @@ const Pagination = ({
                 <button
                     key={page}
                     className={`px-3 py-2 cursor-pointer  ${page === currentPage
-                        ? "text-black"
-                        : "text-black/50"
+                        ? "text-black font-black"
+                        : "text-black/30"
                         }`}
                     onClick={() => onPageChange(page)}
                 >
@@ -35,7 +37,7 @@ const Pagination = ({
             ))}
 
             <button
-                className="px-3 py-2 bg-black/5 text-black disabled:opacity-20"
+                className={`${commonClasses}`}
                 disabled={currentPage === totalPages}
                 onClick={() => onPageChange(currentPage + 1)}
             >
