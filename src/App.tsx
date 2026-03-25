0080dc063fbb73447b0120896f39471bec0ab991
NYKFAN <nemesnyik.zsolt@gmail.com>
2026-03-25 19:25:22 +0100

    scroll to top on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import PageRecipe from "./pages/PageRecipe";
 import PageSavedMeals from "./pages/PageSavedMeals";
 import PageLoader from "./components/loader/PageLoader";
 import Page404 from "./pages/Page404";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <PageLoader />
       <BrowserRouter>
+        <ScrollToTop />
         <Header />
         <main className="min-h-screen flex flex-col container mx-auto p-4 py-20 relative z-10">
           <Routes>
